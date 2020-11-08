d3116705b898f7b68d6338e8752630fffcdae21b
elizabeth1331 <44823989+elizabeth1331@users.noreply.github.com>
2020-11-08 14:57:48 -0600

    Constantes y Variables
    Las guarda en la Hashtale y después se rompe

--- src/proyecto1/metodosDeLectura.java
+++ src/proyecto1/metodosDeLectura.java
@@ -15,11 +15,18 @@ import java.util.StringTokenizer;
  * Esta clase hace el análisis principal del código del archivo .asc
  * @author todas
  */
+
+
+
 public class metodosDeLectura {
     /**
      * En este método se lee el archivo .asc línea por línea para su análisis
      * @param name Nombre del archivo .asc
      */
+    
+    //Se crea HashTable donde guardar las variables o/y constantes
+    Hashtable<String,Integer> variables=new Hashtable();
+    
     public void Lectura(String name){
         //Variable que ayuda a llevar el conteo de líneas que ocupan memoria 
         int numLinea = 0;
@@ -46,7 +53,7 @@ public class metodosDeLectura {
                     numLinea++;
                     error = DifModoDeDireccionamiento(line, m, numLinea, VCE); 
                 }else if(line.contains("EQU")|line.contains("equ")){
-                    //Ir a la parte de constantes
+                    GuardarVariables(line);
                 }else if(line.startsWith("ORG")||line.startsWith("org")){
                     //Asignar el valor de inicio de memoria  
                 }else if(line.startsWith("END")||line.startsWith("end")){
@@ -177,5 +184,83 @@ public class metodosDeLectura {
         }
       return false; 
     }
+    
+    
+    
+    public Hashtable<String,Integer> GuardarVariablesH(String clave, Integer valor){
+        
+        System.out.print("   "+ clave +"---"+ valor+"\n");
+        variables.put(clave,valor);
+        System.out.print("   "+variables);
+        
+        return variables;
+        
+        /*if (variables.containsKey(palabra)){
+               //instruccion=instruccion.concat(palabra);
+               //newLine=newLine.concat(variables.get(palabra));
+               System.out.println(palabra +" No es palabra repetida");
+                                    }*/
+    }
+    
+    
+    public Hashtable<String,Integer> GuardarVariables(String line){
+       //Palabra nos sirve para separar la linea en palabras y contabilizarlas
+                    String palabra, clave="";
+                    Integer valor=0;
+                    int numPalabra=0;
+
+                    //Se leen las palabras de la línea
+                    StringTokenizer st1 = new StringTokenizer (line);
+                    while (st1.hasMoreTokens())
+                    {
+                        palabra = st1.nextToken();
+                        numPalabra++;
+                        String aux="";
+
+                        /*Guarda el nombre de la variable o etiqueta*/
+                        if(numPalabra==1){
+                            clave=palabra=palabra.toUpperCase(); 
+                        }
+
+                        //Guarda el contenido de la variable, quitando el $ en el proceso 
+                        if((numPalabra==3)){
+                            int n=palabra.length();
+
+                            // Se transforma la palabra a cadena
+                            char[] p = palabra.toCharArray();
+                            //Arreglo auxiliar donde guardara sin $
+                            char[] auxP=new  char[n-1];
+                            //proceso de guardado
+                            for (int j = 0; j < n-1; j++){   
+                                auxP[j]=p[j+1];
+                            }
+                             aux = String.valueOf(auxP);
+                             valor=Integer.parseInt(aux);
+
+/*
+                             //En caso de que sea un operando en número hexadecimal
+                            if(palabra.startsWith("$")){
+                                System.out.println(palabra+" Es variable hexadecimal");
+                                valor=Integer.parseInt(palabra);
+                                //newLine=newLine.concat(aux);
+
+                            //En caso de que sea un caracter
+                            }else if(palabra.startsWith("#'")){
+                                //System.out.println(palabra+" Es operando ASCII");
+                                //Se utiliza la cadena aux para separar #' del operando
+                                aux=aux.concat(palabra.substring(2));
+                                //System.out.println(aux+" Es el caracter");
+                                //Se comprueba si el operando es un solo caracter
+
+                            //En caso de que sea un operando en número decimal
+                            }*/
+                        }
+                    }
+                            //Envia a la funcion para guardar en la HashTable, y revisar que no contenga
+                             //otra constante o variable con el mismo nombre
+                             GuardarVariablesH(clave,valor);
+                 return variables;
+                    
+}
     
 }
