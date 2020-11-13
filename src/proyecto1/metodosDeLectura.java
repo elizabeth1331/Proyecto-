4770c9b0b74113c3890e3e40eebdf0c58a24f173
elizabeth1331 <44823989+elizabeth1331@users.noreply.github.com>
2020-11-13 16:26:36 -0600

    Hola

--- src/proyecto1/metodosDeLectura.java
+++ src/proyecto1/metodosDeLectura.java
@@ -5,8 +5,11 @@
  */
 package proyecto1;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Scanner;
 import java.util.StringTokenizer;
@@ -29,7 +32,7 @@ public class metodosDeLectura {
     
     public void Lectura(String name){
         //Variable que ayuda a llevar el conteo de líneas que ocupan memoria 
-        int numLinea = 0;
+        int numLinea = 0, numPalabra=0;
         String error = "";
         
         /*Se crean varios archivos dentro de la carpeta del proyecto, cada archivo guarda una lista de las
@@ -44,48 +47,60 @@ public class metodosDeLectura {
         String line = "";
         try {
             file = new Scanner(new FileReader(name));
+            int num=0;
             while (file.hasNextLine()) {
+                num++;
                 line = file.nextLine();
+                numPalabra++;
                 System.out.println(line);
                 if(line.startsWith("*")){
                     //Se salta a la sig línea
                 }else if(line.startsWith("\t")|line.startsWith(" ")){
                     numLinea++;
                     error = DifModoDeDireccionamiento(line, m, numLinea, VCE); 
-                }else if(line.contains("EQU")|line.contains("equ")){
+                }else if(line.contains("EQU")||line.contains("equ")){
                     GuardarVariables(line);
                 }else if(line.startsWith("ORG")||line.startsWith("org")){
                     //Asignar el valor de inicio de memoria  
-                }else if(line.startsWith("END")||line.startsWith("end")){
+                }else if(line.equals("END")||line.equals("end")){
                     //Terminar la lectura del archivo?
-                }else {
+                }else if(!line.equals("")){
                     //Con este objeto podemos dividir la cadena en sub cadenas. 
                     StringTokenizer st = new StringTokenizer (line);
                     //Leemos la ínea hasta encontrar el primer espacio
                     String palabra = st.nextToken();
                     //Convertimos la palabra en mayúsculas 
                     palabra = palabra.toUpperCase();
-                    
-                    if(EsInstruccion(palabra, m)){
+                    if(numPalabra==1){
+                        if(EsInstruccion(palabra, m)!=0){
                         //Error 009
                         //Pero debe revisar todos los archivos para corroborar que la primera "palabra" sea una instrucción,
                         //Si no la encuentra entonces es una ETIQUETA     
                         error = "Error 09: INSTRUCCIÓN CARECE DE AL MENOS UN ESPACIO RELATIVO AL MARGEN"; 
-                    }else{
-                        palabra = "";
-                        //Con este objeto podemos dividir la cadena en sub cadenas. 
-                        st = new StringTokenizer (line);
-                        //Leemos la ínea hasta encontrar el primer espacio
-                        palabra = st.nextToken();
-                        VCE.agregarEtiqueta(palabra, numLinea + 1);                    
+                        }else{
+                            palabra = "";
+                            //Con este objeto podemos dividir la cadena en sub cadenas. 
+                            st = new StringTokenizer (line);
+                            //Leemos la ínea hasta encontrar el primer espacio
+                            palabra = st.nextToken();
+                            VCE.agregarEtiqueta(palabra, numLinea + 1);                    
+                        }
                     }
                 }
-                    
+            try {
+                    FileWriter fstream = new FileWriter("Archivo.txt", true);
+                    BufferedWriter out = new BufferedWriter(fstream);
+                    out.write(num + error);
+                    out.close();
+                } catch (IOException ex) {
+                    System.out.println("Error: "+ex.getMessage());
+                }
             }
             if(line.equals("END")||line.equals("end")){
                 error = "Error 010: NO SE ENCUENTRA END";
             }
             file.close();
+            
         }
         catch (FileNotFoundException e){
             System.out.println("Error al leer el archivo, " + e.getMessage());
@@ -98,28 +113,22 @@ public class metodosDeLectura {
      * @return newLine Es la línea que se analizó más su OPCODE o el error detectado.
      */
     public String DifModoDeDireccionamiento(String line, Mnemonicos m, int numLinea, Var_Cons_Etiq VCE){
-        String newLine;
+        Inmediato IMM= new Inmediato();
+        Relativo REL=new Relativo();
+        Inherente INH=new Inherente();
+        String palabra="";
+        String linea;
+        //Directo y Extendido
         
-        if(line.contains("#")){
-            //Si la línea contiene un #, se utiliza la clase correspondiente al método de direccionamiento inmediato.
-            Inmediato INM= new Inmediato();
-            newLine=INM.AnalizarLinea(line, m);
-            //Si se regresa un error al analizar la línea, se devuelve la cadena con el error
-            if(newLine.contains("Error")){
-                return newLine;
-            /*Si no se encuentra un error, se agrega el código frente al OPCODE dejando un espacio de 3 tabuladores
-            y se regresan ambos*/
-            }else{
-                newLine=newLine.concat("\t\t\t"+line);
-                return newLine;
-            }
-        }else if(line.contains(",")){
-            
+        String newLine="";
+        int op;
+        
+        if(line.contains(",")){
             
             if(line.contains(",X")||line.contains(",x")){
                 //Si la línea contiene es de tipo ",x" o ",X", se utiliza la clase del método de direccionamiento indexado.
                 IndexadoX IND= new IndexadoX();
-                newLine=IND.revisarLineaX(line, m);
+                newLine=IND.revisarLineaX(line, m, variables);
                 //Si se regresa un error al analizar la línea, se devuelve la cadena con el error
                 if(newLine.contains("Error")){
                     return newLine;
@@ -134,7 +143,7 @@ public class metodosDeLectura {
                 
                 //Si la línea contiene es de tipo ",y" o ",Y", se utiliza la clase del método de direccionamiento indexado.
                 IndexadoY IND= new IndexadoY();
-                newLine=IND.revisarLineaY(line, m);
+                linea=newLine=IND.revisarLineaY(line, m, variables);
                 //Si se regresa un error al analizar la línea, se devuelve la cadena con el error
                 if(newLine.contains("Error")){
                     return newLine;
@@ -145,62 +154,81 @@ public class metodosDeLectura {
                     return newLine;
                 }
             }
-        
-        }else{
-            Relativo REL = new Relativo();
-            newLine = REL.RevisarLinea(line, m, VCE, numLinea);
-            Inherente INH = new Inherente();
-            newLine = INH.AnalizarLinea(line, m);
         }
-        return null;
+        else if(!line.equals("")){
+            StringTokenizer st = new StringTokenizer (line);
+            //Leemos la ínea hasta encontrar el primer espacio
+            if(st.hasMoreTokens()){
+                palabra = st.nextToken();
+            }
+            //Convertimos la palabra en mayúsculas 
+            palabra = palabra.toUpperCase();
+            op=EsInstruccion(palabra,m);
+            switch (op){
+                case 0:
+                    System.out.println("Error 004: MNEMÓNICO INEXISTENTE");
+                    newLine="Error 004: MNEMÓNICO INEXISTENTE";
+                    break;
+                case 1:
+                    System.out.println(palabra+" Es instrucción del modo inmediato, directo o extendido xd");
+                    newLine=IMM.AnalizarLinea(line, m, variables);
+                    break;
+                case 2:
+                    System.out.println(palabra+" Es unstrucción del modo inherente");
+                    newLine=INH.AnalizarLinea(line, m);
+                    break;
+                case 3:
+                    System.out.println(palabra+" Es instrucción del modo relativo");
+                    newLine=REL.RevisarLinea(line, m, VCE, numLinea);
+                    break;
+                case 4:
+                    //Puede ser directo o extendido
+                    System.out.println(palabra+" Es instrucción del modo inmediato, directo o extendido xd");
+                    newLine=IMM.AnalizarLinea(line, m, variables);
+                    break;
+                }
+        }
+        return newLine;
     }
 
-    public boolean EsInstruccion(String palabra, Mnemonicos m) {
+    public int EsInstruccion(String palabra, Mnemonicos m) {
+        int op;
         
         //Se recuperan las tablas de todos los modos de direccionamiento 
         System.out.println("La palabra a diferenciar es:"+palabra);
         
         Hashtable<String, String> Inmediato = new Hashtable();
-        Hashtable<String,String> Inherente;
+        Hashtable<String,String> Inherente = new Hashtable();
         Hashtable<String, String> Relativo = new Hashtable();
         Hashtable<String, String> IndexadoX = new Hashtable();
         Hashtable<String, String> IndexadoY = new Hashtable();
-        //Hashtable<String,String> directoYExtendido;
+        Hashtable<String, String> Directo = new Hashtable();
+        Hashtable<String, String> Extendido = new Hashtable();
         
         Inmediato = m.LeerOpcode("ListaInmediato.txt");
         Relativo = m.LeerOpcode("ListaRelativo.txt");
         Inherente = m.LeerOpcode("ListaInherente.txt");
         IndexadoX= m.LeerOpcode("ListaIndexadoX.txt");
         IndexadoY=m.LeerOpcode("ListaIndexadoY.txt");
-        //Añadir los métodos para recuperar el resto de Mnemónicos
+        Directo=m.LeerOpcode("ListaDirecto.txt");
+        Extendido=m.LeerOpcode("ListaExtendido.txt");
         
-        
-        //Comprobamos si la palabra corresponde a algúna instrucción 
-        if (Inmediato.containsKey(palabra)||Relativo.containsKey(palabra)){
-            System.out.println("Es instrucción");
-            return true;
+        if(Inmediato.containsKey(palabra)){
+            op=1;
+        }else if(Inherente.containsKey(palabra)){
+            op=2;
+        }else if(Relativo.containsKey(palabra)){
+            op=3;
+        }else if(Directo.containsKey(palabra)||Inmediato.containsKey(palabra)||Extendido.containsKey(palabra)){
+            //Puede ser directo o extendido
+            op=4;
         }else{
-            System.out.println("No es instrucción");
+            op=0;
         }
-      return false; 
+        return op;
+        //Comprobamos si la palabra corresponde a algúna instrucción 
+         
     }
-    
-    
-    public Hashtable<String,Integer> GuardarVariablesH(String clave, Integer valor){
-        
-        System.out.print("   "+ clave +"---"+ valor+"\n");
-        variables.put(clave,valor);
-        System.out.print("   "+variables);
-        
-        return variables;
-        
-        /*if (variables.containsKey(palabra)){
-               //instruccion=instruccion.concat(palabra);
-               //newLine=newLine.concat(variables.get(palabra));
-               System.out.println(palabra +" No es palabra repetida");
-                                    }*/
-    }
-    
     
     public Hashtable<String,Integer> GuardarVariables(String line){
        //Palabra nos sirve para separar la linea en palabras y contabilizarlas
@@ -233,35 +261,29 @@ public class metodosDeLectura {
                             for (int j = 0; j < n-1; j++){   
                                 auxP[j]=p[j+1];
                             }
-                             aux = String.valueOf(auxP);
-                             valor=Integer.parseInt(aux);
-
-/*
-                             //En caso de que sea un operando en número hexadecimal
-                            if(palabra.startsWith("$")){
-                                System.out.println(palabra+" Es variable hexadecimal");
-                                valor=Integer.parseInt(palabra);
-                                //newLine=newLine.concat(aux);
-
-                            //En caso de que sea un caracter
-                            }else if(palabra.startsWith("#'")){
-                                //System.out.println(palabra+" Es operando ASCII");
-                                //Se utiliza la cadena aux para separar #' del operando
-                                aux=aux.concat(palabra.substring(2));
-                                //System.out.println(aux+" Es el caracter");
-                                //Se comprueba si el operando es un solo caracter
-
-                            //En caso de que sea un operando en número decimal
-                            }*/
+                            aux = String.valueOf(auxP);
+                            valor=Integer.parseInt(aux);
                         }
                     }
-                            //Envia a la funcion para guardar en la HashTable, y revisar que no contenga
-                             //otra constante o variable con el mismo nombre
-                             GuardarVariablesH(clave,valor);
-                 return variables;
+                    //Envia a la funcion para guardar en la HashTable, y revisar que no contenga
+                    //otra constante o variable con el mismo nombre
+                    GuardarVariablesH(clave,valor);
+                return variables;
                     
 }
-    
+    public Hashtable<String,Integer> GuardarVariablesH(String clave, Integer valor){
+        
+        System.out.print("\n   "+ clave +"---"+ valor+"\n");
+        if(variables.containsKey(clave)){
+            variables.replace(clave, valor);
+        }else{
+            variables.put(clave,valor);
+        }
+        System.out.print("\nVariables al momento:\n   "+variables);
+        
+        return variables;
+        
+    }
     
 
 }
